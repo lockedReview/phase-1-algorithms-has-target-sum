4d093fbdfa0ca30876ffe8f44e78afb33bfc05ea
Alex Sanchez <aa.sanchez.dev@gmail.com>
2024-07-02 02:43:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+   
+  for (i=0; i < array.length; i++){
+    console.log(array[i]);
+    let number = array[i];
+    let newArray = array.slice(0, i).concat(array.slice(i + 1));
+   console.log(newArray);
+    let total="";
+
+    for(j=0; j < newArray.length; j++){
+      total = number + newArray[j];
+      if (total === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+
+O(n^2)
 */
 
 /* 
@@ -12,6 +32,7 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  im taking the array and iterating through it. i copy the array and delete the item. i do another iteration that adds the original number with the other numbers in that are in the new array. returns true is equals the target
 */
 
 // You can run `node index.js` to view these console logs
